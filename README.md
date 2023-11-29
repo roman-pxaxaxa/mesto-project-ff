02e9b8e18d40067782a9a6bb50270fda4377ab1c
Roman Nikitin <roman-pxaxaxa@yandex.ru>
2023-11-30 00:34:27 +0300

    Add six initial cards

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
 
+https://github.com/roman-pxaxaxa/mesto-project-ff
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
-// @todo: Темплейт карточки
+const cardsList = document.querySelector('.places__list'); // list of cards
+const cardTemplate = document.querySelector('#card-template').content; // card template
 
-// @todo: DOM узлы
+function prepareCard(cardData, removeCallback) { // add card function
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  cardElement.querySelector('.card__image').src = cardData.link;
+  cardElement.querySelector('.card__image').alt = cardData.name;
+  cardElement.querySelector('.card__title').textContent  = cardData.name;
 
-// @todo: Функция создания карточки
+  const removeButton = cardElement.querySelector('.card__delete-button');
+  removeButton.addEventListener('click', (event) => {
+    removeCallback(event.target);
+  });
 
-// @todo: Функция удаления карточки
+  return cardElement;
+}
 
-// @todo: Вывести карточки на страницу
+function addCard(cardElement) {
+  cardsList.append(cardElement);
+}
+
+function removeCard(removeButton) { // remove card callback
+  const cardToRemove = removeButton.closest('.card');
+  cardToRemove.remove();
+}
+
+initialCards.forEach((card_data) => { // add all cards from array loop
+  addCard(prepareCard(card_data, removeCard));
+});
